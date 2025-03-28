e6d17fb68db52af07b4934bd4e0234e5309be78c
Thaissa Rodrigues <thaissavitoria16705@gmail.com>
2025-03-28 01:32:57 +0000

    Configuração inicial do servidor com MongoDB

---
+++ wedding-backend/server.js
@@ -0,0 +1,30 @@
+import process from "process";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+// Conectar ao MONGODB
+
+mongoose
+.connect (process.env.MONGO_URI)
+.then(() => console.log("MongoDB conectado"))
+.catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
+
+// Rota Inicial
+
+app.get("/", (req, res)=> {
+    res.send("Servidor rodando...");
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
